fd6aa8bdd10540ff5b87f8acbf22d9a66c669cc9
hindehalhoul <hind.halhoul.2003@gmail.com>
2023-12-23 23:55:30 +0100

    init project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { RegistrationComponent } from '../pages/registration/registration.component';
+
+export const routes: Routes = [
+
+    { path: 'registration', component: RegistrationComponent },
+];
---
+++ src/pages/registration/registration.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.scss'
+})
+export class RegistrationComponent {
+  nom: string = '';
+
+}
